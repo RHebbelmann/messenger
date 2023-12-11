23a7c3a9474d32fadeacad2334674e99fcf240af
Hebbelmann <123360231+RHebbelmann@users.noreply.github.com>
2023-12-11 10:14:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.setLedColor(0x00ff00)
     radio.sendString("Hallo")
+    basic.pause(500)
+    basic.setLedColor(0xff0000)
 })
 radio.onReceivedString(function (receivedString) {
+    basic.setLedColor(0x007fff)
     basic.showString(receivedString)
+    basic.pause(500)
+    basic.setLedColor(0xff0000)
 })
-radio.setGroup(1)
+radio.setGroup(169)
+basic.setLedColor(0xff0000)
 basic.forever(function () {
 	
 })
